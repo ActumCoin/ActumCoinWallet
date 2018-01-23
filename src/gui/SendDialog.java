1cbbbab8b2bbfcb11a20e3a15e398976d23e81ce
rshanedev <ryansshane@rocketmail.com>
2018-01-22 21:42:27 -0500

    Add files via upload

--- src/gui/SendDialog.java
+++ src/gui/SendDialog.java
@@ -26,13 +26,15 @@ public class SendDialog {
 		int result = JOptionPane.showOptionDialog(null, panel, "Enter amount of ActumCoin",
 				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
 
-		if (result == JOptionPane.YES_OPTION && !amountField.getText().isEmpty() && isNumeric(amountField.getText())) {
+		if (result == JOptionPane.YES_OPTION && !amountField.getText().isEmpty() && isNumeric(amountField.getText())
+				&& new BigDecimal(amountField.getText()).compareTo(BigDecimal.ZERO) == 1) {
 			amount = new BigDecimal(amountField.getText());
 			address = (String) JOptionPane.showInputDialog(panel,
 					"<html>Paste address to send &#164;" + amountField.getText() + "to.</html>",
 					"Paste destination wallet address", JOptionPane.PLAIN_MESSAGE, null, null, "");
 		} else if (result == JOptionPane.YES_OPTION) {
-			JOptionPane.showMessageDialog(panel, "<html>Enter a <u>number</u> above 0</html>", "Error", JOptionPane.WARNING_MESSAGE);
+			JOptionPane.showMessageDialog(panel, "<html>Enter a <u>number</u> above 0</html>", "Error",
+					JOptionPane.WARNING_MESSAGE);
 			repeat = true;
 		}
 	}
