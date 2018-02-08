692ce6a5ea8bb72586930ea620c3a7a27ab8e1cb
rshanedev <ryansshane@rocketmail.com>
2018-02-08 15:37:51 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,9 +6,12 @@ import java.util.List;
 
 import balance.Balance;
 import balance.Balances;
+import balance.SendManager;
 import gui.GUI;
+import gui.QR;
 import multichain.command.MultiChainCommand;
 import multichain.command.MultichainException;
+import multichain.object.BalanceAsset;
 import util.Preferences;
 
 public class Main {
@@ -16,14 +19,16 @@ public class Main {
 	public static void main(String[] args) {
 		// init
 		new Preferences();
+
 		// connect to blockchain
-		String ip = "192.168.56.1";
-		String port = "6716";
+		String ip = "localhost"; // <-will change lol
+		String port = "6462";
 		String user = "multichainrpc";
-		String pass = "G8LRqDQNtdSRsDiaoZ2wZ3tps24LDMzUq1Ygww9g4YGy";
+		String pass = "HdEvYTGhbHo457TmmN2FZWZfHbMegeFehPVEdKy8VJGi";
 		MultiChainCommand m = new MultiChainCommand(ip, port, user, pass);
-		
-		// get addresses
+
+
+		// get address
 		List<String> addresses = null;
 		try {
 			addresses = m.getAddressCommand().getAddresses();
@@ -31,18 +36,31 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		// balances
+		String address = null;
+
+		for (String a : addresses) {
+			address = a;
+		}
+		
+		SendManager sm = new SendManager(m, address);
+
+		/* balances */
+		// get balances
+		List<BalanceAsset> balancesList = null;
+		try {
+			balancesList = m.getBalanceCommand().getTotalBalances();
+		} catch (MultichainException e) {
+			e.printStackTrace();
+		}
+		
 		List<Balance> b = new ArrayList<Balance>();
-		b.add(new Balance("tst", createBigD(154, 0)));
-		b.add(new Balance("lol", createBigD(23, 2342)));
-		b.add(new Balance("fun", createBigD(69, 420)));
-		b.add(new Balance("wtf", createBigD(666, 666)));
-		b.add(new Balance("fun", createBigD(69, 420)));
-		b.add(new Balance("acm", createBigD(1, 19823)));
+		for (BalanceAsset ba : balancesList) {
+			b.add(new Balance(ba.getName(), new BigDecimal(ba.getQty())));
+		}
 
 		Balances balances = new Balances(b);
 
-		new GUI(balances);
+		new GUI(balances, address, sm);
 
 	}
 
