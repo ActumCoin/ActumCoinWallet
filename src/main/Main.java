e5dbc7e492c5f7b1a228481ca345c63c90db9923
codegreen <ryansshane@rocketmail.com>
2018-04-03 16:42:33 -0400

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
@@ -18,22 +21,53 @@ import multichain.object.BalanceAsset;
 public class Main {
 
 	public static void main(String[] args) {
+		String ip   = "localhost";
+		String port = "2688";
+		String user = "multichainrpc";
+		String pass = "";
+
+		File conf;
+
+		// find the os
+		String os = System.getProperty("os.name");
+		
+		// set the conf file path based on os
+		if (os.substring(0, Math.min(os.length(), 7)).equals("Windows")) {
+			// windows
+			conf = new File(System.getenv("APPDATA") + "\\MultiChain\\testacm\\multichain.conf");
+		} else {
+			// linux/mac
+			conf = new File("~/.multichain/testacm/multichain.conf");
+		}
+
+		// get username and password for multichainrpc
+		try {
+		    Scanner scanner = new Scanner(conf);
+
+		    int lineNum = 0;
+		    while (scanner.hasNextLine()) {
+		        String line = scanner.nextLine();
+		        lineNum++;
+		        if (line.substring(0, Math.min(line.length(), 8)).equals("rpcuser=")) {
+		        	user = line.substring(8, line.length());
+		        	System.out.println(user);
+		        } else if (line.substring(0, Math.min(line.length(), 12)).equals("rpcpassword=")) { 
+		            pass = line.substring(12, line.length());
+		            System.out.println(pass);
+		        }
+		    }
+		} catch(FileNotFoundException e) { 
+		    e.printStackTrace();
+		}
+		
 		// connect to blockchain
 		try {
-			Runtime.getRuntime().exec("multichaind e -daemon");
+			Runtime.getRuntime().exec("multichaind testacm");
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
-		String ip = "localhost";
-		String port = "2688";
-		String user = "multichainrpc";
-		String pass = "6gPTcFXitC617RxEucprgxJXYwyiL2bQYUqi3cs4PAtM";
 		MultiChainCommand m = new MultiChainCommand(ip, port, user, pass);
-
-		String warning = null;
-		
-		
 
 		GUI.createInstance(m);
 
